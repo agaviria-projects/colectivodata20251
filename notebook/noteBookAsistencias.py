40c9c949f9a623e4f3742ca8c8249f157b47d917
yuliana <yuli.mejiap@gmail.com>
2025-04-22 20:54:17 -0500

    se realizan diferntes comandos

--- notebook/noteBookAsistencias.py
+++ notebook/noteBookAsistencias.py
@@ -2,4 +2,11 @@ import pandas as pd
 
 #Leyendo los datos de asistencias
 asistenciaDataFrame=pd.read_csv("./data/asistencia_estudiantes_completo.csv")
-print(asistenciaDataFrame)
+#print(asistenciaDataFrame)
+
+#Obteniendo informacion basica del dataframe
+#print(asistenciaDataFrame.info())
+#print(asistenciaDataFrame.head(20))
+#print(asistenciaDataFrame.describe())
+print(asistenciaDataFrame['estrato'].value_counts())
+
